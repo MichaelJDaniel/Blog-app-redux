2cf45b8ea76e265c42b594df7b02cb86fca3febe
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-12-17 13:52:07 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BlogForm from './components/BlogForm';
+import BlogList from './components/BlogList';
+import Footer from './components/Footer'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/blog/:id" component={Blog}/>
+  </Switch>
+   
+  
 }
-
 export default App;
